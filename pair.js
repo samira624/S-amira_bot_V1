5d67d7964aedd19fc17a42c823d16ee00c7e8b51
samira624 <dileepprasanna929@gmail.com>
2025-03-10 09:11:39 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -85,7 +85,7 @@ router.get("/", async (req, res) => {
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://apero.vn/mindsync",
               },
               caption: sid,
             });
